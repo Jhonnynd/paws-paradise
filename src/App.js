8f0441c96027217abeca52fc38ea91ec1cec667b
Jhonnynd <jhonnynd@gmail.com>
2023-06-08 11:48:36 +0800

    initialized and added MUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Container>
+        <Box sx={{ mt: "50px" }}>
+          <Button>Hi</Button>
+        </Box>
+
+        <Box
+          sx={{
+            mt: "50px",
+            width: "100%",
+            height: "70%",
+            bgcolor: "red",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Hello
+        </Box>
+      </Container>
     </div>
   );
 }
